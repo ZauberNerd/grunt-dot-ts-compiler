7a8079ab60d8e5b66e6e560d7fadc1a973ab5e98
ZauberNerd <zaubernerd@zaubernerd.de>
2014-06-21 19:44:56 +0200

    removed commonjs/AMD wrapper and added typescript wrapper

--- src/Compiler.js
+++ src/Compiler.js
@@ -15,9 +15,7 @@ var grunt = require('grunt')
 var Compiler = function(opt) {
   this.opt = _.defaults(opt || {}, {
     variable  : 'tmpl',
-    node      : false,
     root      : opt.gruntRoot,
-    requirejs : false,
     key       : function(filepath) {
       return path.basename(filepath, path.extname(filepath));
     }
@@ -153,55 +151,16 @@ Compiler.prototype.getFileContent = function(filePath) {
 Compiler.prototype.compileTemplates = function(files) {
 
   var js = '', _this = this;
-  
 
-  // RequireJS
-  if(!this.opt.requirejs && !this.opt.node) {
-    if(this.opt.variable.indexOf('.') !== -1) {
-      js += this.opt.variable + ' = (function(){' + grunt.util.linefeed;
-    } else {
-      js += 'var ' + this.opt.variable + ' = (function(){' + grunt.util.linefeed;
-    }
-  }
-
-  if(this.opt.requirejs && !this.opt.node) {
-    js += 'if(typeof define !== \'function\') {' + grunt.util.linefeed;
-    js += '  define = require(\'amdefine\')(module);' + grunt.util.linefeed;
-    js += '}' + grunt.util.linefeed;
-  }
-
-  if(this.opt.requirejs) {
-    js += 'define(function() {' + grunt.util.linefeed;
-  }
-
-  // Defining encodeHTML method for the templates
-  js += 'function encodeHTMLSource() {';
-  js += '  var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", \'"\': \'&#34;\', "\'": \'&#39;\', "/": \'&#47;\' },';
-  js += '  matchHTML = /&(?!#?\w+;)|<|>|"|\'|\\//g;';
-  js += '  return function() {';
-  js += '    return this ? this.replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : this;';
-  js += '  };';
-  js += '};' + grunt.util.linefeed;
-  js += 'String.prototype.encodeHTML=encodeHTMLSource();' + grunt.util.linefeed;
-
-  js += 'var tmpl = {};' + grunt.util.linefeed;
+  js =+ 'export var ' + this.opt.variable
+      + ': { [name: string]: (any) => string } = {};' + grunt.util.linefeed;
 
   files.map(function(filePath) {
     var template = _this.getFileContent(filePath)
       , fn       = doT.template(template)
       , key      = _this.opt.key(filePath);
-    js += '  tmpl' + "['" + key + "']=" + fn + ';' + grunt.util.linefeed;
+    js += 'tmpl' + "['" + key + "'] = " + fn + ';' + grunt.util.linefeed;
   });
-
-  if(!this.opt.requirejs && !this.opt.node) {
-    js += 'return tmpl;})();';
-  } else if(this.opt.requirejs) {
-    js += 'return tmpl;});' + grunt.util.linefeed;
-  } else if(this.opt.simple && this.opt.node){
-    js += '';
-  } else if(this.opt.node) {
-    js += 'module.exports = tmpl;';
-  }
 
   return js;
 
